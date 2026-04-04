cf4b3a944f3a7dabc78ac63e55e2fbe313ba7a00
matthiasverlaecktax <matthias.verlaeckt@anticimex.be>
2026-04-05 00:35:19 +0200

    debug: add webhook_test endpoint to diagnose handle_comment errors

--- backend/apps/engagement/urls.py
+++ backend/apps/engagement/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.engagement.views import facebook_webhook
+from apps.engagement.views import facebook_webhook, webhook_test
 
 urlpatterns = [
     path('webhooks/facebook/', facebook_webhook, name='facebook_webhook'),
+    path('webhooks/test/', webhook_test, name='webhook_test'),
 ]
